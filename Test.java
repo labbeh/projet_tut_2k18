9bf0c309d3c1a8eda03c8f71d96695b7eddd4264
labbeh <hugo.labbe@etu.univ-lehavre.fr>
2018-06-18 11:28:29 +0200

    changements main

--- Test.java
+++ Test.java
@@ -12,7 +12,8 @@ package projet_tut_2018;
 public class Test {
     public static void main(String[] args)
     {
-        System.out.println("ok");
+        //System.out.println("ok");
+        System.out.println("ligne2");
     }
     
 }
