ba4875e6ab3e06483fc82657a66e99908f71dd93
labbeh <hugo.labbe@etu.univ-lehavre.fr>
2018-06-18 16:57:03 +0200

    ajout des classes ihm frame et panel kd

---
+++ kingdomino/ihm/PanelPlateau.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet_tut_2018.kingdomino.ihm;
+
+import java.awt.GridLayout;
+import javax.swing.*;
+/**
+ *
+ * @author labbeh
+ */
+public class PanelPlateau extends JPanel
+{
+    public PanelPlateau()
+    {
+        this.setLayout(new GridLayout(4,4));
+    }
+}
