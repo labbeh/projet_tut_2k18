62ebe15d7030d7dc3244de4ee3e204d9ef71502f
AriKuso <tenoinemessi@hotmail.fr>
2018-06-20 12:16:57 +0200

    Utilisation des setBoutton

--- Santorini/ihm/PanelPlateau.java
+++ Santorini/ihm/PanelPlateau.java
@@ -7,6 +7,8 @@ package Santorini.ihm;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import Santorini.Controleur;
 
 /**
  *
@@ -14,26 +16,34 @@ import java.awt.*;
  */
 public class PanelPlateau extends JPanel
 {
+    private Controleur ctrl;
     private static final int NB_CASES = 5;
     
-    private JButton[][] cases;
+    private BouttonCase[][] cases;
     
-    public PanelPlateau()
+    public PanelPlateau(Controleur ctrl)
     {
+        this.ctrl = ctrl;
         this.setLayout(new GridLayout(PanelPlateau.NB_CASES,PanelPlateau.NB_CASES));
         
-        this.cases = new JButton[PanelPlateau.NB_CASES][PanelPlateau.NB_CASES];
+        this.cases = new BouttonCase[PanelPlateau.NB_CASES][PanelPlateau.NB_CASES];
         
         for(int cptLig=0; cptLig<this.cases.length; cptLig++)
             for(int cptCol=0; cptCol<this.cases.length; cptCol++)
             {
-                this.cases[cptLig][cptCol] = new JButton();
+                this.cases[cptLig][cptCol] = new BouttonCase(cptLig,cptCol);
                 this.add(this.cases[cptLig][cptCol]);
             }
     }
-    
+    public JButton getBouton(int posLig, int posCol){return this.cases[posLig][posCol];}
     public void setImage(String url, int posLig, int posCol)
     {
         this.cases[posLig][posCol].setIcon(new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource(url))));
+    }
+    public void actionPerformed(ActionEvent evt)
+    {
+        BouttonCase btn = (BouttonCase)evt.getSource();
+        ctrl.setColBoutton(btn.getPosCol());
+        ctrl.setLigBoutton(btn.getPosLig());
     }
 }
