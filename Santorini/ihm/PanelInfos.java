b61731d513219f78d82f344a3d027a386c71466e
labbeh <hugo.labbe@etu.univ-lehavre.fr>
2018-06-21 16:17:26 +0200

    texte au centre du panel et couleurs joueurs

--- Santorini/ihm/PanelInfos.java
+++ Santorini/ihm/PanelInfos.java
@@ -11,30 +11,36 @@ import java.awt.*;
 
 public class PanelInfos extends JPanel
 {
-    private static String JOUEUR1 = "Joueur 1";
-    private static String JOUEUR2 = "Joueur 2";
+    private static String joueur1 = "Joueur 1";
+    private static String joueur2 = "Joueur 2";
     
     private JLabel lbJoueur1;
     private JLabel lbJoueur2;
+    private JLabel lbMsg    ;
     
     /**
      * Permet l'affichage du nom des joueurs en haut de la fenêtre
      */
     public PanelInfos(String nomJ1, String nomJ2)
     {
-        PanelInfos.JOUEUR1 = nomJ1;
-        PanelInfos.JOUEUR2 = nomJ2;
+        PanelInfos.joueur1 = nomJ1;
+        PanelInfos.joueur2 = nomJ2;
                 
         this.setLayout(new BorderLayout());
         
         this.lbJoueur1 = new JLabel();
         this.lbJoueur2 = new JLabel();
+        this.lbMsg     = new JLabel();
         
-        this.lbJoueur1.setText(PanelInfos.JOUEUR1);
-        this.lbJoueur2.setText(PanelInfos.JOUEUR2);
+        this.lbJoueur1.setText(PanelInfos.joueur1);
+        this.lbJoueur2.setText(PanelInfos.joueur2);
         
-        this.add(this.lbJoueur1, BorderLayout.WEST);
-        this.add(this.lbJoueur2, BorderLayout.EAST);
+        this.add(this.lbJoueur1, BorderLayout.WEST  );
+        this.add(this.lbMsg    , BorderLayout.CENTER);
+        this.add(this.lbJoueur2, BorderLayout.EAST  );
+        
+        this.lbJoueur1.setForeground(Color.BLUE); // new Color(r,g,b)
+        this.lbJoueur2.setForeground(Color.RED);
     }
     /**
      * Permet de mettre en évidence (avec une couleur) le joueur qui doit jouer
@@ -44,13 +50,15 @@ public class PanelInfos extends JPanel
     {
         if(numJoueur == 1)
         {
-            this.lbJoueur1.setText("<html><h3>" +PanelInfos.JOUEUR1+ "</h3></html>");
-            this.lbJoueur2.setText(PanelInfos.JOUEUR2);
+            this.lbJoueur1.setText("<html><h3>" +PanelInfos.joueur1+ "</h3></html>");
+            this.lbJoueur2.setText(PanelInfos.joueur2);
         }
         else
         {
-            this.lbJoueur2.setText("<html><h3>" +PanelInfos.JOUEUR2+ "</h3></html>");
-            this.lbJoueur1.setText(PanelInfos.JOUEUR1);
+            this.lbJoueur2.setText("<html><h3>" +PanelInfos.joueur2+ "</h3></html>");
+            this.lbJoueur1.setText(PanelInfos.joueur1);
         }
     }
+    
+    public void setText(String txt){ this.lbMsg.setText(txt); }
 }
